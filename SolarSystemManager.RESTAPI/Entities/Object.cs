dcdd75284c834d1275bd300992e51ee333f4498d
Ryan Granquist <rmg61103@gmail.com>
2024-02-09 14:56:01 -0600

    added folders and example objects.

---
+++ SolarSystemManager.RESTAPI/Entities/Object.cs
@@ -0,0 +1,19 @@
+﻿namespace SolarSystemManager.RESTAPI.Entities
+{
+    public class Object
+    {
+        /// <summary>
+        /// The id of the object
+        /// </summary>
+        private int id { get; set; }
+        /// <summary>
+        /// Name of the object
+        /// </summary>
+        private string name { get; set; }
+
+        /// <summary>
+        /// Optional parameter
+        /// </summary>
+        private string? optionalParam{ get; set; }
+    }
+}

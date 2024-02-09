f9ba15fff19cd03d84c38c38b071c95e9c1ddfbd
Ryan Granquist <rmg61103@gmail.com>
2024-02-09 16:10:01 -0600

    Imported vue prime and prime flex. Started login page.

--- SolarSystemManager.WEB/src/views/LoginView.vue
+++ SolarSystemManager.WEB/src/views/LoginView.vue
@@ -1,14 +1,34 @@
 <template>
-  <div>Login</div>
+  <Card>
+    <template #title>Login</template>
+    <template #content>
+      <div class="flex flex-column gap-2">
+        <label for="username">Username</label>
+        <InputText id="username" v-model="username" style="width: 250px" />
+        <label for="password">Password</label>
+        <Password id="password" v-model="password" />
+      </div>
+    </template>
+  </Card>
+
+  <Button>Hello</Button>
+
+  {{ password }}
 </template>
 
 <script setup lang="ts">
+import InputText from 'primevue/inputtext'
+import Password from 'primevue/password'
+import Button from 'primevue/button'
+import Card from 'primevue/card'
+
+import { ref } from 'vue'
+
 console.log('code here')
+const username = ref('')
+const password = ref('')
 </script>
 
 <style scoped>
 /* css here */
-div {
-  color: red;
-}
 </style>

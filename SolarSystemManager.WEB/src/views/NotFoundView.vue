10bc278fb6040fc0c759aad91205cb4c0479c4fa
Ryan Granquist <rmg61103@gmail.com>
2024-02-13 00:37:12 -0600

    Cooked up a banger 404 page

--- SolarSystemManager.WEB/src/views/NotFoundView.vue
+++ SolarSystemManager.WEB/src/views/NotFoundView.vue
@@ -1,14 +1,47 @@
 <template>
-  <div>Not Found</div>
+  <div>
+    <HeaderBar></HeaderBar>
+    <img
+      src="../assets/Images/openStars.jpg"
+      alt="open space"
+      style="width: 100%; height: 100%; position: fixed; object-fit: cover"
+    />
+    <div
+      @mouseover="hover = true"
+      style="
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        text-align: center;
+      "
+    >
+      <transition name="fade" mode="out-in">
+        <template v-if="hover">
+          <RouterLink to="/" style="color: white; text-decoration: none">Take Me Home</RouterLink>
+        </template>
+        <template v-else>
+          <RouterLink to="/" style="color: white; text-decoration: none">You're Lost...</RouterLink>
+        </template>
+      </transition>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-console.log('code here')
+import { ref } from 'vue'
+import HeaderBar from '@/components/HeaderBar.vue'
+
+const hover = ref(false)
 </script>
 
 <style scoped>
-/* css here */
-div {
-  color: red;
+.fade-enter-active,
+.fade-leave-active {
+  transition: 1s -webkit-filter linear;
+}
+.fade-enter-from,
+.fade-leave-to {
+  -webkit-filter: blur(20px);
 }
 </style>

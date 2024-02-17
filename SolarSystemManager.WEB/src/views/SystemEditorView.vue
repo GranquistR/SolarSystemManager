2daab571a8b13646992a90649553eca49fe27fa5
kirchnerleo <kirchnera2707@my.uwstout.edu>
2024-02-17 12:12:12 -0600

    call for AddSpaceObject

--- SolarSystemManager.WEB/src/views/SystemEditorView.vue
+++ SolarSystemManager.WEB/src/views/SystemEditorView.vue
@@ -2,16 +2,24 @@
   <HeaderBar require-login></HeaderBar>
   <div>Editor</div>
   {{ currentSolarSystem }}
+  
+
 </template>
 
 <script setup lang="ts">
-import HeaderBar from '@/components/Header/HeaderBar.vue'
 import SolarSystemService from '@/services/SolarSystemService'
 import { ref } from 'vue'
+import HeaderBar from '@/components/Header/HeaderBar.vue'
 
 const currentSolarSystem = ref<any>([])
 
+//will eventually get solar system from data base by id
 SolarSystemService.GetSolarSystemById(1).then((response) => {
   currentSolarSystem.value = response
 })
+
+SolarSystemService.AddSpaceObject("Star").then((response) => {
+  currentSolarSystem.value = response
+})
+
 </script>

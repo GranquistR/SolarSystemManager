53e70d73cacf315a1b1b536c474240ced8fb0f1e
Jadenheart101 <87915721+Jadenheart101@users.noreply.github.com>
2024-02-18 11:19:52 -0600

    Added temp bg image to landing page

--- SolarSystemManager.WEB/src/views/LandingPageView.vue
+++ SolarSystemManager.WEB/src/views/LandingPageView.vue
@@ -1,17 +1,30 @@
 <template>
   <div>
     <HeaderBar></HeaderBar>
+  </div>
+  <div class="overflow-hidden">
+    <div class="bg-cover bg-center border-round h-screen w-full" style="background-image: url('https://images3.alphacoders.com/130/1300409.jpg');">
+
+   
     <h1>landing</h1>
     <p>Home page content</p>
     {{ allSolarSystems }}
+
+    
+    </div>
   </div>
 
   <!-- Background image -->
-  <div class="flex flex-wrap align-items-center justify-content-center">
+  <!-- <div class="flex flex-wrap align-items-center justify-content-center">
     <div class="border-round bg-primary-100 w-full h-screen p-3 m-3">
-
+     
     </div>
-  </div>
+  </div> -->
+  <!-- <div class="overflow-hidden">
+    <div class="bg-cover bg-center border-round h-screen w-full" style="background-image: url('https://images3.alphacoders.com/130/1300409.jpg');">
+      
+    </div>
+  </div> -->
 </template>
 
 <script setup lang="ts">

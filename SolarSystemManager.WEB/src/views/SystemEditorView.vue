43c6e7e3edbbbab1ae35499f503f88ace519ff17
kirchnerleo <kirchnera2707@my.uwstout.edu>
2024-03-08 18:23:02 -0600

    Update SystemEditorView.vue
    Added more to the gui to include add and remove buttons

--- SolarSystemManager.WEB/src/views/SystemEditorView.vue
+++ SolarSystemManager.WEB/src/views/SystemEditorView.vue
@@ -1,26 +1,55 @@
 <template>
+  <!--Formatting is ugly as sin but will fix next sprint-->
   <HeaderBar require-login></HeaderBar>
   <div>Editor</div>
-  {{ currentSolarSystem.systemName }} <!--JSON output :(-->
-  <!--I know it doesn't look pretty I just want it to work-->
-  <div class="flex justify-content-center">
-    <div class="flex-column flex w-9">
-      <Card class="mb-4">
-        <template #title> Add or remove stars and planets to your solar system </template>
-        <template #content>
-            <Button> Add space object </Button>
-            <Button> Remove space object </Button>
-          <DataTable :value="currentSolarSystem.spaceObjects">
-            <Column field="objectName" header="Name"></Column>
-            <Column field="objectType" header="Type"></Column>
-            <Column field="xCoord" header="X Coordinte"></Column>
-            <Column field="yCoord" header="Y Coordinate"></Column>
-            <Column field="objectSize" header="Size"></Column>
-            <Column field="objectColor" header="Color"></Column>
-          </DataTable>
-        </template>
-      </Card>
-    </div>
+  <div> Welcome to {{ currentSolarSystem.systemName }} </div>
+  <div class="flex-column flex w-9">
+    <Card class="mb-4">
+      <template #title>Add an object to {{currentSolarSystem.systemName}}</template>
+      <template #content>
+        <div>
+          <p1>Name: Earth</p1><br>
+          <p1>Type: Planet</p1><br>
+          <p1>X coordinate: 000</p1><br>
+          <p1>Y coordinate: 000</p1><br>
+          <label for="size">Size: </label>
+          <InputText variant="filled" id="size" v-model="objectSize" style="width: 245px" />
+          <br><p1>Color: #5DE2E7</p1>
+        </div>
+        <Button>Add Object</Button>
+      </template>
+    </Card>
+  </div>
+
+  <div class="flex-column flex w-9">
+    <Card class="mb-4">
+      <template #title>Remove an object from {{currentSolarSystem.systemName}}</template>
+      <template #content>
+        <div>
+          <label for="size">Object ID: </label>
+          <InputText variant="filled" id="size" v-model="removeObjectByID" style="width: 245px" />
+        </div>
+        <Button>Remove object</Button>
+      </template>
+    </Card>
+  </div>
+
+  <div class="flex-column flex w-9">
+    <Card class="mb-4">
+      <template #title>Objects in this solar system</template>
+      {{ currentSolarSystem.spaceObjects }}
+      <template #content>
+        <DataTable :value="currentSolarSystem.spaceObjects">
+          <Column field="spaceObjectID" header="Database ID"></Column><!--For testing purposes only-->
+          <Column field="objectName" header="Name"></Column>
+          <Column field="objectType" header="Type"></Column>
+          <Column field="xCoord" header="X Coordinte"></Column>
+          <Column field="yCoord" header="Y Coordinate"></Column>
+          <Column field="objectSize" header="Size"></Column>
+          <Column field="objectColor" header="Color"></Column>
+        </DataTable>
+      </template>
+    </Card>
   </div>
 </template>
 
@@ -32,8 +61,11 @@ import Card from 'primevue/card'
 import Button from 'primevue/button'
 import DataTable from 'primevue/datatable'
 import Column from 'primevue/column'
+import InputText from 'primevue/inputtext'
 
 const currentSolarSystem = ref<any>([])
+const objectSize = ref('')
+const removeObjectByID = ref('')
 
 SolarSystemService.GetSolarSystemByID(22).then((response) => {
   currentSolarSystem.value = response
@@ -41,7 +73,7 @@ SolarSystemService.GetSolarSystemByID(22).then((response) => {
 
 const test = ref<string>("")
 
-SolarSystemService.AddSpaceObject(1).then((response) => {
+SolarSystemService.AddSpaceObject(objectSize).then((response) => { //this works right?
   test.value = response
 })
 </script>

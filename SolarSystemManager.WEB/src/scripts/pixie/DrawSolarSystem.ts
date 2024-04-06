b332ba14d7751e67cad49fb21f1f517800943654
Jadenheart101 <87915721+Jadenheart101@users.noreply.github.com>
2024-04-06 15:33:37 -0500

    All sprites
    all sprites are added and displaying

--- SolarSystemManager.WEB/src/scripts/pixie/DrawSolarSystem.ts
+++ SolarSystemManager.WEB/src/scripts/pixie/DrawSolarSystem.ts
@@ -24,10 +24,42 @@ export default class gGraphics {
     //for every object in the solar system
     solarSystem.spaceObjects.forEach((element: any) => {
       //sets the sprite
-      const sprite = Sprite.from('/src/assets/Images/sprites/gasPlanet.png')
+      let sprite = Sprite.from('/src/assets/Images/sprites/rockyRingedPlanet.png')
+
+      //checks the object type and changes the sprite accordingly
+      //Planets
+      if(element.objectType == 'Water Planet') sprite = Sprite.from('/src/assets/Images/sprites/waterPlanet.png')
+      else if(element.objectType == 'Rocky Planet') sprite = Sprite.from('/src/assets/Images/sprites/rockyPlanet.png')
+      else if(element.objectType == 'Gas Planet') sprite = Sprite.from('/src/assets/Images/sprites/gasPlanet.png')
+      else if(element.objectType == 'Icy Planet') sprite = Sprite.from('/src/assets/Images/sprites/icyPlanet.png')
+      else if(element.objectType == 'Lava Planet') sprite = Sprite.from('/src/assets/Images/sprites/lavaPlanet.png')
+      else if(element.objectType == 'Crater Planet') sprite = Sprite.from('/src/assets/Images/sprites/craterPlanet.png')
+      else if(element.objectType == 'Earthlike Planet') sprite = Sprite.from('/src/assets/Images/sprites/earthlikePlanet.png')
+      else if(element.objectType == 'Water Ringed Planet') sprite = Sprite.from('/src/assets/Images/sprites/waterRingedPlanet.png')
+      else if(element.objectType == 'Rocky Ringed Planet') sprite = Sprite.from('/src/assets/Images/sprites/rockyRingedPlanet.png')
+      else if(element.objectType == 'Gas Ringed Planet') sprite = Sprite.from('/src/assets/Images/sprites/gasRingedPlanet.png')
+      else if(element.objectType == 'Icy Ringed Planet') sprite = Sprite.from('/src/assets/Images/sprites/icyRingedPlanet.png')
+      else if(element.objectType == 'Lava Ringed Planet') sprite = Sprite.from('/src/assets/Images/sprites/lavaRingedPlanet.png')
+      else if(element.objectType == 'Crater Ringed Planet') sprite = Sprite.from('/src/assets/Images/sprites/craterRingedPlanet.png')
+      else if(element.objectType == 'Earthlike Ringed Planet') sprite = Sprite.from('/src/assets/Images/sprites/earthlikeRingedPlanet.png')
+    //Space Objects
+      else if(element.objectType == 'Rocky Asteroid') sprite = Sprite.from('/src/assets/Images/sprites/rockyAsteroid.png')
+      else if(element.objectType == 'Icy Asteroid') sprite = Sprite.from('/src/assets/Images/sprites/icyAsteroid.png')
+      else if(element.objectType == 'Lava Asteroid') sprite = Sprite.from('/src/assets/Images/sprites/lavaAsteroid.png')
+      else if(element.objectType == 'Crater Asteroid') sprite = Sprite.from('/src/assets/Images/sprites/craterAsteroid.png')
+      else if(element.objectType == 'Star') sprite = Sprite.from('/src/assets/Images/sprites/star.png')
+      else if(element.objectType == 'Neutron Star') sprite = Sprite.from('/src/assets/Images/sprites/neutronStar.png')
+      else if(element.objectType == 'Black Hole') sprite = Sprite.from('/src/assets/Images/sprites/blackHole.png')
+      else if(element.objectType == 'Comet') sprite = Sprite.from('/src/assets/Images/sprites/comet.png')
+      else console.log('Error: Object type not found')
+
+      
+
+
+
       //sets the sprite properties
       sprite.anchor.set(0.5)
-      sprite.scale.set(element.objectSize * (1 / 512), element.objectSize * (1 / 512))
+      sprite.scale.set(element.objectSize * (1 / 1024), element.objectSize * (1 / 1024))
       sprite.position.set(element.xCoord, element.yCoord)
       sprite.tint = element.objectColor
       sprite.filters = this.defaultFilters

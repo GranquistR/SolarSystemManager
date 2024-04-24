61ef65c38a6297e18d0f890146e0226e5cd67069
Michael Ma <mat8230@my.uwstout.edu>
2024-04-24 02:45:22 -0500

    Updated preview column

--- SolarSystemManager.WEB/src/views/DashboardView.vue
+++ SolarSystemManager.WEB/src/views/DashboardView.vue
@@ -13,7 +13,7 @@
         <template #subtitle> The universe is yours to explore </template>
         <template #content>
           <RouterLink to="/new">
-            <Button> Create System </Button>
+            <Button style="font-weight: bold; font-size: 1.3em; color:ivory"> Create System </Button>
           </RouterLink>
         </template>
       </Card>
@@ -25,31 +25,27 @@
 
           <DataTable
             selectionMode="single" headerStyle="width: 3rem"
-            :value="solarSystems"
+            :value="processedSolarSystems"
             @row-click="(row) => ViewerGoTo(row.data.systemId)"
-            v-model:selection="selectedSystems" dataKey="systemId"
             >
-          
-            <!--Group Delete button-->
-
-            <Column selectionMode="multiple" headerStyle="width: 3rem"></Column>
 
             <Column header="Name">
               <template #body="slotProps">
-                <span class="highlight-name">{{ slotProps.data.systemName }}</span>
+                <span 
+                v-tooltip.top="'Clicke to view and edit'"
+                class="highlight-name">{{ slotProps.data.systemName }}</span>
               </template>
             </Column>
 
             <Column header="Preview">
-            <template #body="slotProps">
-            <div class="col">
-              <SpaceObjectDisplay :spaceObject="slotProps.data.spaceObjects[1]" :size="30" />
-              <SpaceObjectDisplay :spaceObject="slotProps.data.spaceObjects[2]" :size="30" />
-              <SpaceObjectDisplay :spaceObject="slotProps.data.spaceObjects[3]" :size="30" />
-              <SpaceObjectDisplay :spaceObject="slotProps.data.spaceObjects[4]" :size="30" />
-            </div>
-            </template>
-
+              <template #body="slotProps">
+                  <SpaceObjectDisplay
+                    v-for="(object, index) in slotProps.data.previewObjects"
+                    :key="index"
+                    :spaceObject="object"
+                    :size="60"
+                  />
+              </template>
             </Column>
 
             <!--New Column: Total bodies in each system.-->
@@ -79,17 +75,18 @@
 
             <!--Delete button-->
             <Column header="Delete">
-            <template #body="slotProps">
-            <Button icon="pi pi-trash" class="p-button-rounded p-button-danger" @click="confirmDelete(slotProps.data.systemId)" />
-            </template>
+              <template #body="slotProps">
+              <Button icon="pi pi-trash" class="p-button-rounded p-button-danger" @click="confirmDelete(slotProps.data.systemId)" />
+              </template>
             </Column>
             <!--End Delete button-->
           </DataTable>
 
           <!--Delete system dialog-->
           <Dialog v-model:visible="deleteDialogVisible" :closable="false">
-          <p>Are you sure you want to delete <strong style="color: #f44336; font-size: 1.3em;">
-            {{ systemNameToDelete }}</strong> ?</p>
+            <p>Are you sure you want to delete <strong style="color: #f44336; font-size: 1.3em;">
+              {{ systemNameToDelete }}</strong> ?
+            </p>
             <template #footer>
               <Button label="Cancel" icon="pi pi-times" @click="deleteDialogVisible = false" />
               <Button label="Yes" icon="pi pi-check" @click="deleteSolarSystem" />
@@ -126,6 +123,7 @@
 </style>
 
 <script setup lang="ts">
+import { onMounted, watch } from 'vue';
 import { RouterLink } from 'vue-router'
 import Card from 'primevue/card'
 import HeaderBar from '@/components/Header/HeaderBar.vue'
@@ -141,18 +139,15 @@ import User from '@/Entities/User'
 import SolarSystem from '@/Entities/SolarSystem'
 import CustomMessage from '@/components/CustomMessage.vue'
 import SpaceObjectDisplay from '@/components/ViewerUi/SpaceObjectDisplay.vue'
-//import ToggleButton from 'primevue/togglebutton';
-
-
 
 const user = inject<User | null>('currentUser');
-const solarSystems = ref<any>([])
+const solarSystems = ref([])
 const deleteDialogVisible = ref(false)
 const systemIdToDelete = ref<number | null>(null);
 const message = ref()
 const systemNameToDelete = ref('');
-const selectedSystems = ref('');
-
+//const selectedSystems = ref('');
+const processedSolarSystems = ref([]);
 
 const confirmDelete = (systemId: number) => {
   const system = solarSystems.value.find((s: SolarSystem) => s.systemId === systemId);
@@ -160,6 +155,47 @@ const confirmDelete = (systemId: number) => {
   systemIdToDelete.value = systemId;
   deleteDialogVisible.value = true;
 };
+
+//Function to process the solarSystems and update processedSolarSystems
+function processSolarSystems() {
+  processedSolarSystems.value = solarSystems.value.map(solarSystem => {
+    //Shallow copy of the solar system
+    const processedSystem = { ...solarSystem };
+    
+    //If there are more than 6 objects, pick 6 at random, otherwise use all available
+    processedSystem.previewObjects = solarSystem.spaceObjects.length > 6
+      ? getRandomObjects(solarSystem.spaceObjects, 6)
+      : solarSystem.spaceObjects;
+    
+    return processedSystem;
+  });
+}
+
+//Function to get random objects serving as a preview
+function getRandomObjects(objects, count) {
+  const shuffled = objects.sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, count);
+}
+
+//Watch the solarSystems for changes and reprocess
+watch(solarSystems, () => {
+  processSolarSystems();
+});
+
+//Call processSolarSystems initially on component mount
+onMounted(() => {
+  if (user) {
+    SolarSystemService.GetUserSolarSystems(user)
+      .then((response) => {
+        if (response.success === false) {
+          throw new Error('Failed to load solar systems');
+        }
+        solarSystems.value = response.data;
+        //Process the data for previews
+        processSolarSystems();
+      });
+  }
+});
 
 //Shows user owned solar systems
 if (user){
